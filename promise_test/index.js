0d1b3896692da93586064c55459d7501d8a2df09
liqkm <liqkm@yonyou.com>
2020-05-28 22:02:53 +0800

    async/await的介绍

--- promise_test/index.js
+++ promise_test/index.js
@@ -9,24 +9,24 @@ const path = require('path');
  */
 
 
-//回调方法解决异步
-function getFileContent(fileNmae, callBack) {
-    const fullFileName = path.resolve(__dirname, 'files', fileNmae);
-    fs.readFile(fullFileName, (err, data) => {
-        //data默认是二进制的形式
-        if(err) return console.error(err);
-        callBack && callBack( JSON.parse(data.toString()) );
-    })
-}
-getFileContent('a.json', (aData) => {
-    console.log('a data:', aData);
-    getFileContent(aData.next, (bData) => {
-        console.log('b data:', bData);
-        getFileContent(bData.next, (cData) => {
-            console.log('c data:', cData );
-        })
-    })
-})
+//一：回调方法解决异步
+// function getFileContent(fileNmae, callBack) {
+//     const fullFileName = path.resolve(__dirname, 'files', fileNmae);
+//     fs.readFile(fullFileName, (err, data) => {
+//         //data默认是二进制的形式
+//         if(err) return console.error(err);
+//         callBack && callBack( JSON.parse(data.toString()) );
+//     })
+// }
+// getFileContent('a.json', (aData) => {
+//     console.log('a data:', aData);
+//     getFileContent(aData.next, (bData) => {
+//         console.log('b data:', bData);
+//         getFileContent(bData.next, (cData) => {
+//             console.log('c data:', cData );
+//         })
+//     })
+// })
 
 //promise 方法解决异步的问题
 function promiseGetFileContent(fileName){
@@ -40,15 +40,32 @@ function promiseGetFileContent(fileName){
         })
     })
 }
-promiseGetFileContent('a.json')
-.then( aData => {
-    console.log('promise>a data:', aData);
-    return promiseGetFileContent(aData.next)
-})
-.then( bData => {
-    console.log('promise>b data:', bData);
-    return promiseGetFileContent(bData.next);
-})
-.then( cData => {
-    console.log('promise>c data:', cData);
-})
+//二：promise 方法解决异步的问题
+// promiseGetFileContent('a.json')
+// .then( aData => {
+//     console.log('promise>a data:', aData);
+//     return promiseGetFileContent(aData.next)
+// })
+// .then( bData => {
+//     console.log('promise>b data:', bData);
+//     return promiseGetFileContent(bData.next);
+// })
+// .then( cData => {
+//     console.log('promise>c data:', cData);
+// })
+
+//三： async await的方式解决异步
+async function readFileName(){
+    const aData = await promiseGetFileContent('a.json');
+    console.log('aData:', aData);
+    const bData = await promiseGetFileContent(aData.next);
+    console.log('bData:', bData);
+    const cData = await promiseGetFileContent(bData.next);
+    console.log('cData:', cData);
+}
+readFileName();
+//async await 的总结：
+//1、await后面可以追加promise对象，获取resolve的值
+//2、await必须包裹在async函数里面
+//3、async 函数执行返回的也是一个promise对象
+//4、try-catch可以截获promise中reject的值
